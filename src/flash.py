d0a9f000d1c4d29423375733b2df5dc39822b454
Jean Gressmann <jean@0x42.de>
2020-11-15 08:21:50 +0100

    Initial version working with test device

--- src/flash.py
+++ src/flash.py
@@ -4,6 +4,7 @@ from enum import Enum
 import argparse
 import platform
 import sys
+import time
 
 from can import Message, Notifier, BufferedReader
 import bincopy
@@ -76,17 +77,26 @@ class XCPErrors:
 class XCPFlash:
     """A Tool for flashing devices like electronic control units via the xcp-protocol."""
 
+    BYTE_ORDER_LE = 0
+    BYTE_ORDER_BE = 1
+
     _reader = None
     _bus = None
     _notifier = None
     _tx_id = 0
     _rx_id = 0
     _conn_mode = 0
-    _data_len = 0
-    _max_data_prg = 0
-    _max_data = 8
+    _ag = 1
+    _ag_override = False
+    _byte_order = BYTE_ORDER_LE
+    _queue_size = 0
+    _max_block_size = 0
+    _initial_comm_mode = None
+    _extended_id = False
+    _master_block_mode_supported = False
+    _min_separation_time_us = 0
 
-    def __init__(self, tx_id, rx_id, connection_mode=0, channel=None):
+    def __init__(self, tx_id: int, rx_id: int, connection_mode: int=0, **kwargs):
         """Sets up a CAN bus instance with a filter and a notifier.
 
         :param tx_id:
@@ -95,23 +105,56 @@ class XCPFlash:
             Id for incoming messages
         :param connection_mode:
             Connection mode for the xcp-protocol. Only set if a custom mode is needed.
-        :param channel:
-            The channel for the can adapter. Only needed for Usb2can on Windows.
         """
+
+        channel = kwargs.get("channel", None)
+        self._extended_id = kwargs.get("extended_id", False)
+
+        ag = kwargs.get("ag", 0)
+        if ag <= 0:
+            self._ag_override = False
+        else:
+            self._ag_override = True
+            self._ag = ag
 
         self._reader = BufferedReader()
         if platform.system() == "Windows":
             from can.interfaces.usb2can import Usb2canBus
             self._bus = Usb2canBus(channel=channel)
+        elif platform.system() == "Linux":
+            from can.interfaces.socketcan import SocketcanBus
+            self._bus = SocketcanBus(channel=channel)
         else:
             from can.interface import Bus
-            self._bus = Bus()
+            self._bus = Bus(channel=channel)
         self._bus.set_filters(
-            [{"can_id": rx_id, "can_mask": rx_id + 0x100, "extended": False}])
+            [{"can_id": rx_id, "can_mask": rx_id + 0x100, "extended": self._extended_id}])
         self._notifier = Notifier(self._bus, [self._reader])
         self._tx_id = tx_id
         self._rx_id = rx_id
         self._conn_mode = connection_mode
+        self._initial_comm_mode = connection_mode
+
+    @staticmethod
+    def sys_byte_order():
+        return XCPFlash.BYTE_ORDER_LE if sys.byteorder == 'little' else XCPFlash.BYTE_ORDER_BE
+
+    @staticmethod
+    def swap16(value: int):
+        value &= 0xffff # ensure 16 bit
+        i0 = (value >> 8) & 0xff
+        i1 = value & 0xff
+        return i0 | (i1 << 8)
+
+    @staticmethod
+    def swap32(value: int):
+        value &= 0xffffffff # ensure 32 bit
+        i0 = (value >> 24) & 0xff
+        i1 = (value >> 16) & 0xff
+        i2 = (value >> 0) & 0xff
+        i3 = value & 0xff
+        return i0 | (i1 << 8) | (i2 << 16) | (i3 << 24)
+
 
     @staticmethod
     def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=50, fill='█'):
@@ -194,6 +237,23 @@ class XCPFlash:
             elif msg is not None:
                 self.send_can_msg(msg, False)
 
+    def _wait_for_rx(self, timeout_s: float):
+        time_left_ns = int(timeout_s * 1e9)
+
+        while time_left_ns >= 0:
+
+            start = time.perf_counter_ns()
+            received = self._reader.get_message(time_left_ns * 1e-9)
+            stop = time.perf_counter_ns()
+
+            time_left_ns -= stop - start
+
+            if received is not None and received.arbitration_id == self._rx_id:
+                return received
+
+
+        return None
+
     def execute(self, command, **kwargs):
         """Execute a command
 
@@ -213,42 +273,120 @@ class XCPFlash:
         """
 
         msg = Message(arbitration_id=self._tx_id,
-                      is_extended_id=False, data=bytes(8))
+                      is_extended_id=self._extended_id, data=bytes(8))
         msg.data[0] = command.value
         if command == XCPCommands.CONNECT:
             msg.data[1] = self._conn_mode
-            response = self.send_can_msg(msg)
+            response = None
+
+            while True:
+                self._bus.send(msg)
+                received = self._reader.get_message(0.1) # 100 [ms]
+                if None is received:
+                    pass
+
+                elif received.arbitration_id == self._rx_id:
+                    if received.data[0] == XCPResponses.SUCCESS.value:
+                        response = received
+                        break
+
+                    raise ConnectionAbortedError(received.data[1])
+                else:
+                    pass # some other message
+
+
             self._max_data = response.data[4] << 8
             self._max_data += response.data[5]
+            self._byte_order = response.data[2] & 1
+
+            if not self._ag_override:
+                self._ag = 1 << ((response.data[2] >> 1) & 0x3)
+
+            if self._byte_order == self.sys_byte_order():
+                self._swap16 = lambda x: x & 0xffff
+                self._swap32 = lambda x: x & 0xffffffff
+            else:
+                self._swap16 = self.swap16
+                self._swap32 = self.swap32
+
             return response
+
+        if command == XCPCommands.GET_COMM_MODE_INFO:
+            done = False
+            while not done:
+                self._bus.send(msg)
+                received = self._reader.get_message()
+                if None is received:
+                    break
+                elif received.arbitration_id == self._rx_id:
+                    if received.data[0] == XCPResponses.SUCCESS.value:
+                        #Position                 Type           Description
+                        # 0                       BYTE           Packet ID: 0xFF
+                        # 1                       BYTE           Reserved
+                        # 2                       BYTE           COMM_MODE_OPTIONAL
+                        # 3                       BYTE           Reserved
+                        # 4                       BYTE           MAX_BS
+                        # 5                       BYTE           MIN_ST
+                        # 6                       BYTE           QUEUE_SIZE
+                        # 7                       BYTE           XCP Driver Version Number
+                        self._master_block_mode_supported = (received.data[2] & 1) == 1
+                        self._max_block_size = received.data[4]
+                        self._min_separation_time_us = 100 * received.data[5]
+
+                    break
+                else:
+                    pass # some other message
+
         if command == XCPCommands.DISCONNECT:
-            return self.send_can_msg(msg)
+            # return self.send_can_msg(msg)
+            self._bus.send(msg)
         if command == XCPCommands.SET_MTA:
+            addr = self._swap32(kwargs['addr'])
             msg.data[3] = kwargs.get('addr_ext', 0)
-            for i in range(4, 8):
-                msg.data[i] = (kwargs['addr'] & (
-                    0xFF000000 >> (8 * (i - 4)))) >> (8 * (7 - i))
+            msg.data[4] = addr & 0xff
+            msg.data[5] = (addr >> 8) & 0xff
+            msg.data[6] = (addr >> 16) & 0xff
+            msg.data[7] = (addr >> 24) & 0xff
+
             return self.send_can_msg(msg)
         if command == XCPCommands.PROGRAM_START:
+            # 0 BYTE Packet ID: 0xFF
+            # 1 BYTE Reserved
+            # 2 BYTE COMM_MODE_PGM
+            # 3 BYTE MAX_CTO_PGM [BYTES] Maximum CTO size for PGM
+            # 4 BYTE MAX_BS_PGM
+            # 5 BYTE MIN_ST_PGM
+            # 6 BYTE QUEUE_SIZE_PGM
+
             response = self.send_can_msg(msg)
-            max_dto_prg = response.data[3]
-            max_bs = response.data[4]
-            self._data_len = (max_dto_prg - 2) * max_bs
-            self._max_data_prg = max_dto_prg - 2
+
+            prog_comm_mode = response.data[2]
+            # if prog_comm_mode != self._conn_mode:
+            #     if self._initial_comm_mode != self._conn_mode:
+            #         # prevent a ring around the rosy situation
+            #         raise ConnectionError(f'communication mode already switch once from {self._initial_comm_mode} to {self._conn_mode} with new request for {prog_comm_mode}')
+
+            #     self._conn_mode = prog_comm_mode
+            #     # fix me: do this automatically
+            #     raise ConnectionError(f'device requires comm mode 0x{prog_comm_mode:02x} for programming')
+
+            self._max_block_size = response.data[4]
+            self._max_cto = response.data[3]
+            if self._max_cto == 0 or self._max_cto % self._ag:
+                raise ConnectionError(f'inconsistent device params: MAX_CTO_PGM={self._max_cto}, ADDRESS_GRANULARITY={self._ag}')
+
+            self._queue_size = response.data[6]
+            self._master_block_mode_supported = (response.data[2] & 1) == 1
+            self._min_separation_time_us = 100 * response.data[5]
+
             return response
         if command == XCPCommands.PROGRAM_CLEAR:
-            for i in range(4, 8):
-                msg.data[i] = (kwargs['range'] & (
-                    0xFF000000 >> (8 * (i - 4)))) >> (8 * (7 - i))
-            return self.send_can_msg(msg)
-        if command == XCPCommands.PROGRAM or command == XCPCommands.PROGRAM_NEXT:
-            msg.data[1] = kwargs["size"]
-            position = 2
-            for data in kwargs["data"]:
-                msg.data[position] = data
-                position += 1
-            return self.send_can_msg(msg, kwargs['size'] <= self._max_data_prg)
-        if command == XCPCommands.PROGRAM_RESET:
+            # range = int(self._swap32(kwargs['range']) / self._ag)
+            range = self._swap32(kwargs['range'])
+            msg.data[4] = range & 0xff
+            msg.data[5] = (range >> 8) & 0xff
+            msg.data[6] = (range >> 16) & 0xff
+            msg.data[7] = (range >> 24) & 0xff
             return self.send_can_msg(msg)
 
     def program(self, data):
@@ -260,29 +398,75 @@ class XCPFlash:
             the firmware as byte-array
         """
         print("flashing new firmware...")
-        bytes_send = 0
-        while bytes_send < len(data):
-            send_length = self._data_len
-            if bytes_send % 10000 <= self._data_len:
-                self.print_progress_bar(bytes_send, len(data),
-                                        prefix="Progress:",
-                                        suffix="Complete")
-                sys.stdout.flush()
-            if send_length > len(data) - bytes_send:
-                send_length = len(data) - bytes_send
-            self.execute(XCPCommands.PROGRAM, size=send_length,
-                         data=data[bytes_send:bytes_send + self._max_data_prg])
-            send_length -= self._max_data_prg
-            bytes_send += min(send_length, self._max_data_prg)
-            while send_length > 0:
-                self.execute(XCPCommands.PROGRAM_NEXT, size=send_length,
-                             data=data[bytes_send:bytes_send + self._max_data_prg])
-                bytes_send += min(send_length, self._max_data_prg)
-                send_length -= self._max_data_prg
-        self.print_progress_bar(bytes_send, len(data),
-                                prefix="Progress:",
-                                suffix="Complete")
-        self.execute(XCPCommands.PROGRAM_RESET)
+
+        # 0 BYTE Command Code = 0xD0
+        # 1 Number of data elements [AG] [1..(MAX_CTO-2)/AG]
+        # 2..AG-1 BYTE Used for alignment, only if AG >2
+        # AG=1: 2..MAX_CTO-2 AG>1: AG  MAX_CTO-AG ELEMENT   Data elements
+
+        # The MASTER_BLOCK_MODE flag indicates whether the Master Block Mode is available.
+        # If  the  master  device  block  mode  is  supported,
+        # MAX_BS  indicates  the  maximum  allowed  block  size  as  the  number  of
+        # consecutive  command  packets  (DOWNLOAD_NEXT  or  PROGRAM_NEXT) in a block sequence.
+        # MIN_ST indicates the required minimum separation time between the packets of a block
+        # transfer  from  the  master  device  to  the  slave device in units of 100 microseconds.
+
+        data_elements_per_message = int((self._max_cto - 2) / self._ag)
+        bytes_per_message = data_elements_per_message * self._ag
+        offset = 0
+
+        data_offset_start = 4 if self._ag == 4 else 2
+        msg = Message(arbitration_id=self._tx_id, is_extended_id=self._extended_id, data=bytes(8))
+
+
+        if self._master_block_mode_supported:
+            max_bytes_per_block = self._max_block_size * bytes_per_message
+            bytes_left_in_block = 0
+
+            while offset < len(data):
+                if bytes_left_in_block:
+                    msg.data[0] = XCPCommands.PROGRAM_NEXT.value
+                else:
+                    bytes_left_in_block = min(max_bytes_per_block, len(data) - offset)
+                    msg.data[0] = XCPCommands.PROGRAM.value
+
+                msg_bytes = min(bytes_left_in_block, bytes_per_message)
+                msg.data[1] = bytes_left_in_block
+                bytes_left_in_block -= msg_bytes
+
+
+                msg.data[data_offset_start:data_offset_start + msg_bytes] = data[offset:offset + msg_bytes]
+                offset += msg_bytes
+
+                self._bus.send(msg)
+
+                # wait
+                if self._min_separation_time_us > 0:
+                    time.sleep(self._min_separation_time_us * 1e-6)
+
+                if 0 == bytes_left_in_block:
+                    response = self._wait_for_rx(1)
+
+                    if None is response:
+                        raise ConnectionError('timeout')
+
+                    if response.data[0] != XCPResponses.SUCCESS.value:
+                        raise ConnectionAbortedError(response.data[1])
+
+        else:
+            msg.data[0] = XCPCommands.PROGRAM.value
+            msg.data[1] = data_elements_per_message
+            steps = int(len(data) / bytes_per_message)
+
+            for i in range(0, steps):
+                msg.data[data_offset_start:data_offset_start + bytes_per_message] = data[offset:offset + bytes_per_message]
+                offset += bytes_per_message
+                self.send_can_msg(msg)
+
+        # PROGRAM_RESET is optional, send it and hope for the best
+        msg.data[0] = XCPCommands.PROGRAM_RESET.value
+        self._bus.send(msg)
+
 
     def clear(self, start_addr, length):
         """Clear the memory of the device
@@ -298,20 +482,16 @@ class XCPFlash:
         self.execute(XCPCommands.PROGRAM_START)
         self.execute(XCPCommands.SET_MTA, addr=start_addr)
         self.execute(XCPCommands.PROGRAM_CLEAR, range=length)
+        # self.execute(XCPCommands.SET_MTA, addr=start_addr)
 
     def connect(self):
         """Connect to the device
-
-        :raises: ConnectionError
-            if the device doesn't support flash programming or the address granularity is > 1
         """
         print("connecting...")
         response = self.execute(XCPCommands.CONNECT)
         if not response.data[1] & 0b00010000:
             raise ConnectionError(
                 "Flash programming not supported by the connected device")
-        if response.data[2] & 0b00000110:
-            raise ConnectionError("Address granularity > 1 not supported")
 
     def disconnect(self):
         """Disconnect from the device"""
@@ -331,6 +511,7 @@ class XCPFlash:
 
         try:
             self.connect()
+            #self.execute(XCPCommands.GET_COMM_MODE_INFO)
             self.clear(start_addr, len(data))
             self.program(data)
         except ConnectionAbortedError as err:
@@ -363,12 +544,28 @@ if __name__ == "__main__":
                         help="Connection mode for xcp-session")
     parser.add_argument("--channel", dest="channel", type=str, required=False, default="ED000200",
                         help="Channel for USB2can adapter on Windows")
+    parser.add_argument("--extended", dest="extended", type=int, required=False, default=-1,
+                        help="rx/tx use extended identifiers (CAN 2.0B)")
+    parser.add_argument("--ag", dest="ag", type=int, required=False, default=0,
+                        help="Override address granularity reported by device Values (4, 2, 1).")
     args = parser.parse_args()
 
     f = bincopy.BinFile(args.firmware)
 
-    xcp_flash = XCPFlash(int(args.transmission_id, 16), int(
-        args.response_id, 16), int(args.conn_mode, 16), args.channel)
+    txid = int(args.transmission_id, 16)
+    rxid = int(args.response_id, 16)
+
+    ext = None
+    if args.extended < 0:
+        ext = (txid & ~0x7FF) != 0 or (rxid & ~0x7FF) != 0
+    elif args.extended == 0:
+        ext = False
+    else:
+        ext = True
+
+    xcp_flash = XCPFlash(
+        txid, rxid, int(args.conn_mode, 16),
+        channel=args.channel, extended_id=ext, ag=args.ag)
     xcp_flash(f.minimum_address, f.as_binary())
 
     sys.exit(0)
